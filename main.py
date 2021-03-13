c0e8ee9c645d7b925b420bf66061e35422f6da30
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2021-03-14 01:47:44 +0300

    add get_city_data()
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 from retrieve import get_country_data
 from retrieve import get_asn_data
+from retrieve import get_city_data
 
 if __name__ == '__main__':
     get_country_data()   # retrieves GeoLite2-Country
     get_asn_data()       # retrieves GeoLite2-ASN
+    get_city_data()      # retrieves GeoLite2-City
